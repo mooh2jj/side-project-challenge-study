21a0c4177b620fe7fd9a0b98462bfca736483f29
minbo2002 <minbo2002@naver.com>
2023-06-23 01:09:50 +0900

    [feat]: Tag Mockito 라이브러리 Unit 테스트

--- 김민보/src/test/java/com/study/sidejavachallenge/domain/board/entity/CategoryRepositoryTest.java
+++ 김민보/src/test/java/com/study/sidejavachallenge/domain/board/entity/CategoryRepositoryTest.java
@@ -11,7 +11,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @Transactional
-class CategoryTest {
+class CategoryRepositoryTest {
 
     @Autowired
     private CategoryRepository categoryRepository;;
